d6e06c78f8f7776c441fa2db15f01557665b27d7
BT_2S14_25 <asa@email.com>
2024-09-18 10:20:01 -0400

    Finished calculations and now printing the values and formatting each value as a decimal value that gives the nearest cent.

--- src/Main.java
+++ src/Main.java
@@ -27,17 +27,22 @@ public class Main {
         Scanner ppl = new Scanner(System.in);
         System.out.println("Please enter the number of people: ");
         String numOfPeople = ppl.nextLine();
+        System.out.println();
 
 
         // Calculating values(Tip per person, total bill, total bill per person)
         double totalBill = Double.parseDouble(bill) * tipDecimal + Double.parseDouble(bill);
         double tipPerPerson = totalTip / Integer.parseInt(numOfPeople);
 
-        // Outputting everything
-        System.out.println("Your total bill including tip is: $" + totalBill);
-        System.out.println("Your total tip amount is: $" + totalTip);
-        System.out.println("The total tip per person is: $" + tipPerPerson);
-        System.out.println("Your total per person including tip is: $" + (totalBill/Integer.parseInt(numOfPeople)));
+        // Outputting everything and formatting every value as a decimal so it rounds to the nearest cent
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println("Your total bill including tip is: $" + df.format(totalBill));
+        System.out.println();
+        System.out.println("Your total tip amount is: $" + df.format(totalTip));
+        System.out.println();
+        System.out.println("The total tip per person is: $" + df.format(tipPerPerson));
+        System.out.println();
+        System.out.println("Your total per person including tip is: $" + df.format(totalBill/Integer.parseInt(numOfPeople)));
 
 
 
